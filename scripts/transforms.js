d2075dbc152bc79d8407cd858aa87a6d88d98b66
Hieu Le <hieu.le@stthomas.edu>
2022-04-07 08:34:23 -0500

    fix bugs

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -1,61 +1,61 @@
 // create a 4x4 matrix to the parallel projection / view matrix
 function mat4x4Parallel(prp, srp, vup, clip) {
     // 1. translate PRP to origin
-    let translation = new Matrix(4,4); 
-    Mat4x4Translate(translation,prp.x,prp.y,prp.z); 
-    // 2. rotate VRC such that (u,v,n) align with (x,y,z) 
-    
-    // 3. shear such that CW is on the z-axis
-    // 4. translate near clipping plane to origin
-    // 5. scale such that view volume bounds are ([-1,1], [-1,1], [-1,0])
-
-    // ...
-    // let transform = Matrix.multiply([...]);
-    // return transform;
+    //Modify the given matrix to get "translate PRP to origin" matrix
     var tPRP = new Matrix(4, 4);
     Mat4x4Translate(tPRP, -prp.x, -prp.y, -prp.z);
 
+    // 2. rotate VRC such that (u,v,n) align with (x,y,z)
+    //Modify the given matrix to get "rotate VRC such that (u,v,n) align with (x,y,z)" matrix
     var rVRC = new Matrix(4, 4);
     Mat4x4RotateVRC(rVRC, prp, srp, vup);
 
-    var cw = new Vector3((clip[0] + clip[1])/2, (clip[2] + clip[3])/2, -clip[4]);
+    // 3. shear such that CW is on the z-axis
+    //Center of window: (left+right)/2, (bottom+top)/2
+    var cw = new Vector3((clip[0] + clip[1])/2, (clip[2] + clip[3])/2, 0);
+    //DOP: CW - PRP
     var dop = cw.subtract(prp);
+    //Modify the given matrix to get "shear such that CW is on the z-axis" matrix
     var shPar = new Matrix(4, 4);
     Mat4x4ShearXY(shPar, -dop.x/dop.z, -dop.y/dop.z);
 
+    // 4. translate near clipping plane to origin
+    //Modify the given matrix to get "translate near clipping plane to origin" matrix
     var tPar = new Matrix(4, 4);
-    Mat4x4Translate(tPar, 0, 0, c[4]);
+    Mat4x4Translate(tPar, 0, 0, clip[4]);
 
+    // 5. scale such that view volume bounds are ([-1,1], [-1,1], [-1,0])
+    //Modify the given matrix to get "scale such that view volume" matrix
     var sPar = new Matrix(4, 4);
     Mat4x4Scale(sPar, 2/(clip[1] - clip[0]), 2/(clip[3] - clip[2]), 1/clip[5]);
-    
+
+    // Multiply them all together
     return Matrix.multiply([sPar, tPar, shPar, rVRC, tPRP]);
 }
 
 // create a 4x4 matrix to the perspective projection / view matrix
 function mat4x4Perspective(prp, srp, vup, clip) {
     // 1. translate PRP to origin
-    // 2. rotate VRC such that (u,v,n) align with (x,y,z)
-    // 3. shear such that CW is on the z-axis
-    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
-
-    // ...
-    // let transform = Matrix.multiply([...]);
-    // return transform;
     var tPRP = new Matrix(4, 4);
     Mat4x4Translate(tPRP, -prp.x, -prp.y, -prp.z);
 
+    // 2. rotate VRC such that (u,v,n) align with (x,y,z)
     var rVRC = new Matrix(4, 4);
     Mat4x4RotateVRC(rVRC, prp, srp, vup);
 
+    // 3. shear such that CW is on the z-axis
+    //Center of window: (left+right)/2, (bottom+top)/2
     var cw = new Vector3((clip[0] + clip[1])/2, (clip[2] + clip[3])/2, -clip[4]);
+    //DOP: CW - PRP
     var dop = cw.subtract(prp);
     var shPar = new Matrix(4, 4);
     Mat4x4ShearXY(shPar, -dop.x/dop.z, -dop.y/dop.z);
 
+    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
     var sPer = new Matrix(4, 4);
     Mat4x4Scale(sPer, 2 * clip[4] / ((clip[1] - clip[0]) * clip[5]), 2 * clip[4] / ((clip[3] - clip[2]) * clip[5]), 1 / clip[5]);
-
+    
+    // Multiply them all together
     return Matrix.multiply([sPer, shPar, rVRC, tPRP]);
 }
 
@@ -74,16 +74,22 @@ function mat4x4MPer() {
     let mper = new Matrix(4, 4);
     mper.values =   [[1, 0, 0, 0],
                      [0, 1, 0, 0],
-                     [0, 0, 0, 0],
+                     [0, 0, 1, 0],
                      [0, 0, -1, 0]];
     return mper;
 }
 
+//Helper function to create a 4x4 matrix - Rotated VRC
 function Mat4x4RotateVRC(mat4x4, prp, srp, vup) {
+    //n-axis: normalized(PRP-SRP)
     var n = prp.subtract(srp);
     n.normalize();
+
+    //u-axis: normalized(VUP x n-axis)
     var u = vup.cross(n);
     u.normalize();
+
+    //v-axis: n-axis x u-axis
     var v = n.cross(u);
     mat4x4.values = [[u.x, u.y, u.z, 0], 
                     [v.x, v.y, v.z, 0], 
